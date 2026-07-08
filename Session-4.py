d5548f46585d631380d49b01ca8cfa785aca4ab7
jyotpandya <jyotpandya9a23@gmail.com>
2026-07-08 22:18:42 +0530

    Add files via upload

---
+++ Session-4.py
@@ -0,0 +1,17 @@
+text = "Flipkart-Sale2024"
+text = text.lower()
+text = text.replace("-", " ")
+print(text)
+name = " OnePlus Nord-CE 3 "
+name = name.strip()
+name = name.upper()
+name = name.replace("-", ":")
+print(name)
+word= "ZOMATO-FOOD-2024"
+result = word.split("-")
+print(result)
+text = "Spotify_Premium_Offer"
+print(text[8:15])
+product = "Myntra Shirt"
+price = 799.5
+print("Deal:", product, "is available at ₹", price, "only!")
