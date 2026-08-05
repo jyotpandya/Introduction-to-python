e7dffec1ec08c4be40cf9ed2a18d1d1375b595cb
Jyot-27 <jyotpandya9a23@gmail.com>
2026-08-05 13:10:41 +0530

    Add files via upload

---
+++ Session-2.py
@@ -0,0 +1,24 @@
+#Swiggy order
+print("----Swiggy Order----")#Decolarlation
+item_price=250# 3 VAriable 
+delivery_fee=30
+coupon_discount=30
+print("Item Price-:",item_price)#printing 3 items 
+print("delivery fee-:",delivery_fee)
+print("Coupon Discount-:",coupon_discount)
+
+#Flipkart product's price
+print("----Flipkart Order----")#Decolarlation
+Product=920#variable
+if Product>=1000:#comparation 
+    print("Congratulations,You Are Eligible for Free Dlivery")
+else:
+     print('Delivery charges apply')
+
+print("----IPL MATCH ----")
+team_name="RCB"
+runs_scored=285
+over_played=20
+print("Team Name is :",team_name)
+print("runs_scored-:",runs_scored)
+print("over_played-:",over_played)
