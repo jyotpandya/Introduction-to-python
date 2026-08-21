51a3005d49a6aef6387c53d130a7d3386c808892
Jyot-27 <jyotpandya9a23@gmail.com>
2026-08-21 16:13:21 +0530

    Add files via upload

---
+++ Session17.py
@@ -0,0 +1,23 @@
+print("Task-1")
+import math as plt
+n=225
+f=plt.sqrt(n)
+print(f)
+print("Task-2")
+"""import os
+folder_name = "MyDownloads"
+os.mkdir(folder_name)
+print(os.path.abspath(folder_name))"""
+print("Task-3")
+import datetime as dt
+t=dt.datetime.today().strftime('DD-MM-YYYY HH:MM:SS')
+print(t)
+n=dt.datetime.now()
+print(n)
+print("Task-4")
+import playlist_utils as p
+p.add_song()
+print("Task-5")
+print("Step-1_:Open Terminal")
+print("Step-2_:write python -m venv venv_Tasks")
+print("Step-3_:write pip install requests")
